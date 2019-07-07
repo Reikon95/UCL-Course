01c3c077a1731e1c24fda4082af165b21d334cfb
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-07 13:59:57 +0100

    Update multiplication-table.py

--- multiplication-table.py
+++ multiplication-table.py
@@ -12,4 +12,7 @@ def multitable(max_num, num):
 
 multitable(10, 1)
 
-#generates the values correctly (function within function), need to sort into a table somehow
+# generates the values correctly (function within function), need to sort into a table somehow
+# Ways I could maybe solve: 
+#   Turn them into lists, and then print those lists as ints?
+#   Print the table first?
