96832607839781db27bcc909e909b11022571f67
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-08 18:22:54 +0100

    Update multiplication-table.py

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,18 +1,4 @@
-def multiplication(max_num, num):
-  while num < (max_num + 1):
-    print(num * max_num)
-    num += 1
-
-multiplication(10, 1)
-
-def multitable(max_num, num):
-  while max_num > (num - 1):
-    multiplication(max_num, num)
-    max_num -= 1
-
-multitable(10, 1)
-
-# generates the values correctly (function within function), need to sort into a table somehow
-# Ways I could maybe solve: 
-#   Turn them into lists, and then print those lists as ints?
-#   Print the table first?
+for i in range(1,10):
+    print(str(i) + ":")
+    for j in range(1, 11):
+        print (i*j)
