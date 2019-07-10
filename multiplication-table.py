79fbe1e341126ad35cb25a9dc399d5d6f713f2f1
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-10 19:02:49 +0100

    Update multiplication-table.py

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,7 +1,4 @@
-#we start but creating an empty list and a loop to fill it with numbers desired, in this case 1 - 10
-
 numbers = []
-
 def fill_numbers(max_val):
   for val in range(1, (max_val + 1)):
     numbers.append(val)
@@ -9,17 +6,15 @@ def fill_numbers(max_val):
 fill_numbers(10)
 
 print(numbers)
-
-#now we have the numbers desired in a neat list
+#generates the list of numbers
 
 def multiples():
   for elem in numbers:
     i = 0
     while i < numbers[-1]:
       i += 1
-      print(elem * i)
+      print((elem * i), end=' ') #end makes it horizontal
 
 
 multiples()
-
-#this second function is a nested loop, generates the multiplications of the number up to the last element of numbers, the desired max value
+#nested loop to do the actual maths
