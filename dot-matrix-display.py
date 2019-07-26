092d342a8c1b1e57539da3d303f597ed9343982b
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-26 09:48:59 +0100

    Create dot-matrix-display.py

---
+++ dot-matrix-display.py
@@ -0,0 +1,14 @@
+message = 'This is a completely relevant exercise'
+display_width = 16
+
+message_length = len(message)
+curr_start = 0
+print(message_length)
+
+final = display_width
+first = 0
+while True:
+  print(message[first:final])
+  first += 1
+  final += 1
+  i += 1
