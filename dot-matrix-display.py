52301b0650309a2bc55cdb28f0a2ddd7ef3c084f
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-26 10:28:10 +0100

    Update dot-matrix-display.py

--- dot-matrix-display.py
+++ dot-matrix-display.py
@@ -12,3 +12,26 @@ while True:
   first += 1
   final += 1
   i += 1
+
+   '''
+  
+  INFINITE SCROLL REPEAT
+  
+  '''
+    
+max_display = 20
+beg = 0
+end = max_display
+display = list(message[0:max_display])
+storage = list(message[(max_display + 1):len(message)])
+
+while True:
+  result = ''.join(display)
+  print(result)
+  # print(storage)
+  storage.append(display[0])
+  del display[0]
+  display.append(storage[0])
+  del storage[0]
+
+    
